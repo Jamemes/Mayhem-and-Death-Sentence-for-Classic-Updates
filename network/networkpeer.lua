2d12d9b648a3481c32b8f55a6c6c5d19fdce6c03
Jamemes <56838048+Jamemes@users.noreply.github.com>
2026-02-28 01:07:05 +0300

    4.2
    fixed random crash caused by "server_drop_carry" network message called by NetworkPeer:send(func_name, ...)

--- network/networkpeer.lua
+++ network/networkpeer.lua
@@ -1,20 +1,19 @@
+local func_overrides = {
+	join_request_reply = {5, 5},
+	sync_game_settings = {3, 5},
+	lobby_sync_update_difficulty = {1, "overkill_145"}
+}
 
 Global.HCWP = Global.HCWP or {}
 local data = NetworkPeer.send
 function NetworkPeer:send(func_name, ...)
 	local send_data = {...}
-
-	if not Global.HCWP[self:ip()] and Global.game_settings.difficulty == "easy_wish" then
-		if func_name == "join_request_reply" then
-			send_data[5] = 5
-		elseif func_name == "sync_game_settings" then
-			send_data[3] = 5
-		elseif func_name == "lobby_sync_update_difficulty" then
-			send_data[1] = "overkill_145"
-		end
+	if not Global.HCWP[self:ip()] and Global.game_settings.difficulty == "easy_wish" and func_overrides[func_name] then
+		send_data[func_overrides[func_name][1]] = func_overrides[func_name][2]
+		data(self, func_name, unpack(send_data))
+	else
+		data(self, func_name, ...)
 	end
-
-	data(self, func_name, unpack(send_data))
 
 	if not Global.HCWP[self:ip()] and Global.game_settings.difficulty == "sm_wish" and _G.LuaNetworking:IsHost() then
 		if func_name == "lobby_info" then
